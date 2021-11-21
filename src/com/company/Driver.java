5a79a572adf4a594bb2b663ee8a6e80f17dbf8ea
Mohamed Sayed <mohamedsayedaz18@gmail.com>
2021-11-21 22:30:44 +0200

    Create Admin Functions (Verify & Suspend)

--- src/com/company/Driver.java
+++ src/com/company/Driver.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.sql.*;
+
 public class Driver  extends Person implements Registration,Show_Rating {
     private String National_ID;
     private String Driving_license;
@@ -28,11 +30,34 @@ public class Driver  extends Person implements Registration,Show_Rating {
     int Offer(){
     return 0;
     }
-
-     void Put_FaviorateSource_Areas(){
-
+    int getNationIDByUsername(String username){
+        try {
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/transportation", "root", "");
+            String query = "SELECT National_ID FROM drivers where Username='" + username + "'";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            int national=-1;
+            while (rs.next())
+            {
+               national = rs.getInt("National_ID");
+            }
+            return national;
+        }catch(Exception e){ return -1;}
     }
-
+    void Put_FaviorateSource_Areas(int National_ID,String Location){
+         try{
+             Class.forName("com.mysql.jdbc.Driver");
+             Connection con= DriverManager.getConnection(
+                     "jdbc:mysql://localhost:3306/transportation","root","");
+             PreparedStatement preparedStmt = con.prepareStatement("INSERT into favourite_areas(`Location`, `National`) VALUES (?,?) ");
+             preparedStmt.setString(1,Location);
+             preparedStmt.setInt(2, National_ID);
+             preparedStmt.executeUpdate();
+             con.close();
+         }
+         catch(Exception e){ System.out.println(e);}
+     }
     @Override
     public void Show() {
 
